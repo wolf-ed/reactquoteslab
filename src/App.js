bfd79449bfea3ee43d71efba4042876188c928d9
wolf-ed <wolf7ed@gmail.com>
2021-12-17 18:59:10 +0100

    styles updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React Quotes Lab
-        </p>
+        <h1>React Quotes Lab</h1>
       </header>
     </div>
   );
